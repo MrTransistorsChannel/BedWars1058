4ad496adff613bd8fd2a17cbdcb814409e91c449
MrTransistor <mrtransistor0108@gmail.com>
2023-03-07 00:05:09 +0300

    blockPlace test

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/BreakPlace.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/BreakPlace.java
@@ -158,10 +158,12 @@ public class BreakPlace implements Listener {
                 return;
             }
             if (e.getBlockPlaced().getLocation().getBlockY() >= a.getConfig().getInt(ConfigPath.ARENA_CONFIGURATION_MAX_BUILD_Y)) {
+                System.out.println("maxY");
                 e.setCancelled(true);
                 return;
             }
             if (e.getBlockPlaced().getLocation().getBlockY() <= a.getConfig().getInt(ConfigPath.ARENA_CONFIGURATION_MIN_BUILD_Y)) {
+                System.out.println("minY");
                 e.setCancelled(true);
                 return;
             }
