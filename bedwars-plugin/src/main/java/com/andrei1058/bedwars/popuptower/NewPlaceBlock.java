11c973fc9dd532d9ed86cf032ca4c31259ba353a
MrTransistor <mrtransistor0108@gmail.com>
2023-03-07 21:19:50 +0300

    Fixed popup towers not taking world border into account

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/popuptower/NewPlaceBlock.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/popuptower/NewPlaceBlock.java
@@ -4,6 +4,7 @@ import com.andrei1058.bedwars.BedWars;
 import com.andrei1058.bedwars.api.arena.team.TeamColor;
 import com.andrei1058.bedwars.api.region.Region;
 import com.andrei1058.bedwars.arena.Arena;
+import com.andrei1058.bedwars.arena.Misc;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -16,13 +17,14 @@ public class NewPlaceBlock {
         int y = Integer.parseInt(xyz.split(", ")[1]);
         int z = Integer.parseInt(xyz.split(", ")[2]);
         if (b.getRelative(x, y, z).getType().equals(Material.AIR)) {
-            Iterator var10 = Arena.getArenaByPlayer(p).getRegionsList().iterator();
+            /*Iterator var10 = Arena.getArenaByPlayer(p).getRegionsList().iterator();
             while(var10.hasNext()) {
                 Region r = (Region)var10.next();
                 if (r.isInRegion(b.getRelative(x, y, z).getLocation())) {
                     return;
                 }
-            }
+            }*/
+            if(Misc.isBuildProtected(b.getRelative(x, y, z).getLocation(), Arena.getArenaByPlayer(p))) return;
             if (!ladder) {
                 BedWars.nms.placeTowerBlocks(b, Arena.getArenaByPlayer(p), color, x, y, z);
             } else {
